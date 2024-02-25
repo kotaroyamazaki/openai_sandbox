dc721ad06d03d33e74f3de553b5ebb8d603dea7f
KotaroYamazaki <sentoryoku53m@gmail.com>
2024-02-25 15:58:42 +0900

    fix bug

--- dalle-2/edit.py
+++ dalle-2/edit.py
@@ -9,20 +9,20 @@ import os
 client = OpenAI()
 
 # 入力画像のパス
-input_original_path = "inputs/original.png"
-input_mask_path = "inputs/mask.png"
+input_original_path = "inputs/r.png"
+input_mask_path = "inputs/mask_r.png"
 # input_original_pathとinput_mask_pathの画像を開いて画像が 'RGBA' モードでない場合にのみ変換を行う
 convert_rgba(input_original_path)
 convert_rgba(input_mask_path)
 
 response = client.images.edit(
     model="dall-e-2",
-    image=open("inputs/original.png", "rb"),
-    mask=open("inputs/mask.png", "rb"),
+    image=open(input_original_path, "rb"),
+    mask=open(input_mask_path, "rb"),
     prompt="A sunlit indoor lounge area with a pool containing a penguin",
     n=1,
     size="1024x1024"
 )
 
-result = response.data
-save_image([result], "outputs")
+results = response.data
+save_image(results, "outputs")
