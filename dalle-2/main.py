943a2d50590b8ef5f7d18027e9d84dd433660cf1
KotaroYamazaki <sentoryoku53m@gmail.com>
2024-02-25 12:50:42 +0900

    edit image

--- dalle-2/main.py
+++ dalle-2/main.py
@@ -4,6 +4,9 @@ from os.path import join, dirname
 import requests
 from openai import OpenAI
 from dotenv import load_dotenv
+from PIL import Image
+import os
+
 
 load_dotenv(verbose=True)
 
@@ -18,12 +21,28 @@ client = OpenAI(
     api_key=os.environ["OPENAI_API_KEY"],
 )
 
-response = client.images.generate(
-    model="dall-e-3",
-    prompt="a white siamese cat",
-    size="1024x1024",
-    quality="standard",
+# 入力画像のパス
+input_original_path = "inputs/original.png"
+input_mask_path = "inputs/mask.png"
+
+# input_original_pathとinput_mask_pathの画像を開いて画像が 'RGBA' モードでない場合にのみ変換を行う
+with Image.open(input_original_path) as img:
+    if img.mode != "RGBA":
+        img = img.convert("RGBA")
+        img.save(input_original_path)
+
+with Image.open(input_mask_path) as img:
+    if img.mode != "RGBA":
+        img = img.convert("RGBA")
+        img.save(input_mask_path)
+
+response = client.images.edit(
+    model="dall-e-2",
+    image=open("inputs/original.png", "rb"),
+    mask=open("inputs/mask.png", "rb"),
+    prompt="A sunlit indoor lounge area with a pool containing a penguin",
     n=1,
+    size="1024x1024"
 )
 
 image_url = response.data[0].url
